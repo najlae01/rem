84aa301d4612ac6c15c2a4110cb7391e420a8ddd
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-05-19 03:59:10 +0100

    combined environment textures

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -46,12 +46,12 @@ export default function Experience({
 
   console.log(environment)
 
-  // let ParticlesMixer = new THREE.AnimationMixer(particles.scene)
-  // particles.animations.forEach((clip) => {
-  //   const action = ParticlesMixer.clipAction(clip)
-  //   action.setEffectiveTimeScale(0.2)
-  //   action.play()
-  // })
+  let ParticlesMixer = new THREE.AnimationMixer(particles.scene)
+  particles.animations.forEach((clip) => {
+    const action = ParticlesMixer.clipAction(clip)
+    action.setEffectiveTimeScale(0.2)
+    action.play()
+  })
 
   const navigate = useNavigate()
 
@@ -59,7 +59,7 @@ export default function Experience({
 
   useFrame((state, delta) => {
     RemMixer.update(delta)
-    //ParticlesMixer.update(delta)
+    ParticlesMixer.update(delta)
 
     if (audio.paused && (!animation || !animation.isRunning())) {
       music.play()
@@ -94,6 +94,11 @@ export default function Experience({
       <ambientLight intensity={1.2} />
 
       <OrbitControls />
+      <primitive
+        object={particles.scene}
+        position={[2.4, 0.15, 5.35]}
+        rotation={[-0.3, 0.4, 0.135]}
+      />
       <primitive
         object={rem.scene}
         position={[2.4, 0.35, 5.35]}
